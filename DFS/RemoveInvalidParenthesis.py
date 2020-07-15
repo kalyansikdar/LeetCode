002a9d9a6a9ad69da1bbff66dc3ab5496e91a5fb
Sikdar, Kalyan <kalyan.sikdar@honeywell.com>
2020-07-14 20:49:23 -0400

    Min Remove to make valid parenthesis

--- DFS/RemoveInvalidParenthesis.py
+++ DFS/RemoveInvalidParenthesis.py
@@ -2,17 +2,20 @@ class Solution:
     def removeInvalidParentheses(self, s: str):
         # initialize a set with one element
         # set is used here in order to avoid duplicate element
-        candidates = {s}
+        candidates = set([s])       # Creates a set with the input str as it's only element
+        print (type(candidates), candidates)
         while True:
             valid = []
             for candidate in candidates:
-                if self.isValid(candidate):
+                if self.isValid(candidate):     # Checks if the only element is valid, it's the answer as minimum
+                    # number of paranthesis is to be removed.
                     valid.append(candidate)
+
             if valid:
                 return valid
             # initialize an empty set
             new_candidates = set()
-            # BFS
+            # BFS - finds all the possible candidates, neglects the dups by using set
             for candidate in candidates:
                 for i in range(len(candidate)):
                     new_candidates.add(candidate[:i] + candidate[i + 1:])
