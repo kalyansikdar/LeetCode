d9c1a28eef079489cf9e8500f9963cc155f1db61
Sikdar, Kalyan <kalyan.sikdar@honeywell.com>
2021-01-26 23:15:06 -0500

    LC#82: Remove Duplicates from Sorted List II

--- LinkedList/MergeKSortedLists.py
+++ LinkedList/MergeKSortedLists.py
@@ -23,3 +23,34 @@ class Solution:
             dummy = dummy.next
 
         return head.next
+
+    def mergeKLists_Not_better_TC(self, lists: List[ListNode]) -> ListNode:
+        """
+        Time complexity for heap push is O(logn).
+        Hence overall, for inserting into heap TC is O(n)*O(n)*O(logn) ~ O(n^2*logn)
+        Then, popping from the heap O(logn)
+
+        Overall O(n^2*logn)
+        """
+        if not lists:
+            return
+        if len(lists) == 1:
+            return lists[0]
+
+        heap = []
+        dummy = head = ListNode(None)
+        entryCount = 0
+        for curr in lists:
+            while curr:
+                entryCount += 1  # Tuple comparison breaks for (priority, task) pairs if the priorities are equal and
+                # the tasks do not have a default comparison order. Solution is to store entries as 3-element list
+                # including the priority, an entry count, and the task. The entry count serves as a tie-breaker so
+                # that two tasks with the same priority are returned in the order they were added.
+                heapq.heappush(heap, (curr.val, entryCount, curr))
+                curr = curr.next
+
+        while len(heap) > 0:
+            dummy.next = heapq.heappop(heap)[2]
+            dummy = dummy.next
+
+        return head.next
